c2e546f0af424bd40afcff2dddc60877a6526601
karolinalewicka <karolinalewicka36@gmail.com>
2023-04-14 20:26:41 +0200

    rename SecondActivity and ThirdActivity

--- app/src/main/java/com/example/basicapp/ThirdActivity.kt
+++ app/src/main/java/com/example/basicapp/ThirdActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
