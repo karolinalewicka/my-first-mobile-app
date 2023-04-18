eaec308ecc7291c59c04ab5eba1084ffb2552646
karolinalewicka <karolinalewicka36@gmail.com>
2023-04-18 19:46:10 +0200

    include enums in game logic

--- app/src/main/java/com/example/basicapp/GameActivity.kt
+++ app/src/main/java/com/example/basicapp/GameActivity.kt
@@ -13,6 +13,18 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
     private val boardStatus = Array(3) { IntArray(3) }
     private lateinit var buttons: Array<Array<Button>>
 
+    enum class Position(val row: Int, val col: Int) {
+        POSITION_00(0, 0),
+        POSITION_01(0, 1),
+        POSITION_02(0, 2),
+        POSITION_10(1, 0),
+        POSITION_11(1, 1),
+        POSITION_12(1, 2),
+        POSITION_20(2, 0),
+        POSITION_21(2, 1),
+        POSITION_22(2, 2)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
@@ -40,14 +52,15 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
             return
         }
 
-        val (row, col) = getButtonPosition(view)
+        val position = getPosition(view)
 
         val player = if (player1Turn) 1 else 2
         val symbol = if (player1Turn) "X" else "O"
 
         view.text = symbol
-        boardStatus[row][col] = player
+        boardStatus[position.row][position.col] = player
         roundCount++
+
 
         if (checkForWin()) {
             announceWinner(player)
@@ -62,19 +75,14 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
         player1Turn = !player1Turn
     }
 
-    private fun getButtonPosition(button: Button): Pair<Int, Int> {
-        for (i in buttons.indices) {
-            for (j in buttons[i].indices) {
-                if (button == buttons[i][j]) {
-                    return Pair(i, j)
-                }
+    private fun getPosition(button: Button): Position {
+        for (position in Position.values()) {
+            if (button == buttons[position.row][position.col]) {
+                return position
             }
         }
         throw IllegalArgumentException("Button not found")
     }
-
-
-
 
     private fun checkForWin(): Boolean {
         // Check rows and columns
@@ -88,16 +96,15 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         // Check diagonals
-        if (boardStatus[1][1] != 0) {
-            if (boardStatus[0][0] == boardStatus[1][1] && boardStatus[1][1] == boardStatus[2][2]) {
-                return true // diagonal win
-            }
-            if (boardStatus[0][2] == boardStatus[1][1] && boardStatus[1][1] == boardStatus[2][0]) {
-                return true // diagonal win
-            }
+        if (boardStatus[0][0] != 0 && boardStatus[0][0] == boardStatus[1][1] && boardStatus[1][1] == boardStatus[2][2]) {
+            return true // diagonal 1 win
+        }
+        if (boardStatus[0][2] != 0 && boardStatus[0][2] == boardStatus[1][1] && boardStatus[1][1] == boardStatus[2][0]) {
+            return true // diagonal 2 win
         }
 
-        return false // no win
+        return false
+
     }
 
 
@@ -112,8 +119,6 @@ class GameActivity : AppCompatActivity(), View.OnClickListener {
         }
         resetBoard()
     }
-
-
 
     private fun resetBoard() {
         for (i in 0..2) {
